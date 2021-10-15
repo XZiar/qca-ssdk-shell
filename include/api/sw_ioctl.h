53a2252799415fb584b655b0f0de45269799f840
leiwei <quic_leiwei@quicinc.com>
2021-10-15 17:18:08 +0800

    qca-ssdk-shell: support acl mac entry shell configuration
    Change-Id: I576c4f13e3e8098ee34d1212e2668dddd6c6044c
    Signed-off-by: leiwei <quic_leiwei@quicinc.com>

--- include/api/sw_ioctl.h
+++ include/api/sw_ioctl.h
@@ -1,5 +1,8 @@
 /*
  * Copyright (c) 2014-2019, 2021, The Linux Foundation. All rights reserved.
+ *
+ * Copyright (c) 2022, Qualcomm Innovation Center, Inc. All rights reserved.
+ *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
  * above copyright notice and this permission notice appear in all copies.
@@ -337,6 +340,8 @@ extern "C" {
 #define SW_API_ACL_UDF_PROFILE_CFG_GET		(25  + SW_API_ACL_OFFSET)
 #define SW_API_ACL_VPGROUP_SET			(26  + SW_API_ACL_OFFSET)
 #define SW_API_ACL_VPGROUP_GET			(27  + SW_API_ACL_OFFSET)
+#define SW_API_ACL_MAC_ENTRY_SET		(28  + SW_API_ACL_OFFSET)
+#define SW_API_ACL_MAC_ENTRY_DUMP		(29  + SW_API_ACL_OFFSET)
 
     /*qos*/
 #define SW_API_QOS_OFFSET             500
--- src/shell/shell_config.c
+++ src/shell/shell_config.c
@@ -1,5 +1,8 @@
 /*
  * Copyright (c) 2014-2019, 2021, The Linux Foundation. All rights reserved.
+ *
+ * Copyright (c) 2022, Qualcomm Innovation Center, Inc. All rights reserved.
+ *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
  * above copyright notice and this permission notice appear in all copies.
@@ -550,6 +553,7 @@ struct cmd_des_t gcmd_des[] =
                     SW_API_ACL_VPGROUP_SET, NULL},
             {"vpgroup", "get", "get vpgroup", "<vport_id> <vport_type>",
                     SW_API_ACL_VPGROUP_GET, NULL},
+            {"macentry", "set", "set mac entry", "<entry>", SW_API_ACL_MAC_ENTRY_SET, NULL},
             {NULL, NULL, NULL, NULL, SW_API_INVALID, NULL}/*end of desc*/
         },
     },
@@ -1402,6 +1406,7 @@ struct cmd_des_t gcmd_des[] =
             {"field", "set", "write switch register field", "<reg_addr> <offset> <len> <value> <4>", SW_API_REG_FIELD_SET, NULL},
             {"aclList", "dump", "dump all acl list", "", SW_API_ACL_LIST_DUMP, NULL},
             {"aclRule", "dump", "dump all acl rule", "", SW_API_ACL_RULE_DUMP, NULL},
+	    {"aclMacEntry", "dump", "dump all acl mac entry", "", SW_API_ACL_MAC_ENTRY_DUMP, NULL},
             {"device",  "reset", "reset device",     "", SW_API_SWITCH_RESET, NULL},
             {"module_func",  "set", "set the module function bitmap", "<module>", SW_API_MODULE_FUNC_CTRL_SET, NULL},
             {"module_func",  "get", "set the module function bitmap", "<module>", SW_API_MODULE_FUNC_CTRL_GET, NULL},
